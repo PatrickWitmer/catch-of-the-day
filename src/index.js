ae5450b1b38b94af83884485448dbda58214f82e
WitmerDesigns <patrick@witmerdesigns.com>
2019-09-18 13:08:08 -0600

    linter issues

--- src/index.js
+++ src/index.js
@@ -1,2 +1,10 @@
-// let's go!
-alert('OH SHIT');
+import React from 'react';
+import { render } from 'react-dom';
+
+class StorePicker extends React.Component {
+  render() {
+    return <p>Hello!</p>
+  }
+}
+
+render(<p>Crush your goals like your enemies.</p>, document.querySelector('#main'));
