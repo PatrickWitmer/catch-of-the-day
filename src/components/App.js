3c4581a020fc148e36ed92554611227798f01e87
PatrickWitmer <patrick@witmerdesigns.com>
2019-09-24 18:37:32 -0600

    Added the sample fishes objects for Load Sample Fishes

--- src/components/App.js
+++ src/components/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './Header';
 import Order from './Order';
 import Inventory from './Inventory';
+import sampleFishes from '../sample-fishes';
 
 class App extends React.Component {
   state = {
@@ -15,12 +16,12 @@ class App extends React.Component {
     fishes[`fish${Date.now()}`] = fish;
     //this.state.fishes.push(fish);
     // 3. Set the new fishes object to state
-    this.setState({
-      fishes: fishes
-    });
-
+    this.setState({ fishes });
   };
-  
+  loadSampleFishes = () => {
+    this.setState({ fishes: sampleFishes });
+  }
+
   render() {
     return (
       <div className="catch-of-the-day">
@@ -28,7 +29,7 @@ class App extends React.Component {
           <Header tagline="Fresh Seafood Market" />
         </div>
         <Order />
-        <Inventory addFish={this.addFish} />
+        <Inventory addFish={this.addFish} loadSampleFishes={this.loadSampleFishes} />
       </div>
     );
   }
