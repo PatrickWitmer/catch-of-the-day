78ea756f86c3d2ea3ebdeaca187c3ecccba8c4ee
WitmerDesigns <patrick@witmerdesigns.com>
2019-10-28 15:00:26 -0600

    Local Storage updated

--- src/components/App.js
+++ src/components/App.js
@@ -13,11 +13,20 @@ class App extends React.Component {
   };
   componentDidMount() {
     const { params } = this.props.match;
+    // First reinstate our localStorage
+    const localStorageRef = localStorage.getItem(params.storeId);
+    if(localStorageRef) {
+      this.setState({ order: JSON.parse(localStorageRef )})
+    } 
     this.ref = base.syncState(`${params.storeId}/fishes`, {
       context: this,
       state: 'fishes'
     });
   }
+  componentDidUpdate() {
+    localStorage.setItem(this.props.match.params.storeId, JSON.stringify(this.state.order));
+  }
+
   // Prevent memory leak.
   componentWillUnmount() {
     base.removeBinding(this.ref);
